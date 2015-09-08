3b813b8b90b623b95bbe4adb1c63d0db882ed788
Thomas Nappo <goffpaoli@gmail.com>
2015-09-07 21:33:26 -0400

    Added spacing after commands.

--- easylemp.sh
+++ easylemp.sh
@@ -6,34 +6,45 @@ sleep 4.25
 echo "\033[94mEasyLEMP:\033[0m Updating apt-get...\033[0m"
 sleep 1.5
 sudo apt-get update
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing nginx...\033[0m"
 sleep 1.5
 sudo apt-get install nginx
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing mysql-server...\033[0m"
 sleep 1.5
 sudo apt-get install mysql-server
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing mysql db...\033[0m"
 sleep 1.5
 sudo mysql_install_db
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Securing mysql installation...\033[0m"
 sleep 1.5
 sudo mysql_secure_installation
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing php5-fpm...\033[0m"
 sleep 1.5
 sudo apt-get install php5-fpm
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing php5-mysql...\033[0m"
 sleep 1.5
 sudo apt-get install php5-mysql
+echo ""
 read -p "\033[94mEasyLEMP:\033[0m Press \033[1m[Enter]\033[0m to configure php.ini\033[0m"
 sleep 1.5
 sudo nano /etc/php5/fpm/php.ini
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Restarting php5-fpm...\033[0m"
 sleep 1.5
 sudo service php5-fpm restart
+echo ""
 read -p "\033[94mEasyLEMP:\033[0m Press \033[1m[Enter]\033[0m to configure nginx\033[0m"
 sleep 1.5
 sudo nano /etc/nginx/sites-available/default
+echo ""
 echo "\033[94mEasyLEMP:\033[0m Restarting nginx...\033[0m"
 sleep 1.5
 sudo service nginx restart
+echo ""
 read -p "\033[94mEasyLEMP:\033[0m Done. Press \033[1m[Enter]\033[0m to finish.\033[0m"
