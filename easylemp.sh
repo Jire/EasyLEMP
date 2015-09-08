32dfd1be4272cdc2b68c3f029a1d8a05bd8ab9f8
Thomas Nappo <goffpaoli@gmail.com>
2015-09-07 21:13:24 -0400

    Colors

--- easylemp.sh
+++ easylemp.sh
@@ -1,23 +1,24 @@
-echo "EasyLEMP: Updating apt-get..."
+read -p "\e[34mWelcome to \e[1mBoldEasyLEMP\e[0m\e[34m! Press \e[1m[Enter]\e[0me[34m when you're ready to begin."
+echo "\e[34mEasyLEMP:\e[0m Updating apt-get..."
 sudo apt-get update
-echo "EasyLEMP: Installing nginx..."
+echo "\e[34mEasyLEMP:\e[0m Installing nginx...\n"
 sudo apt-get install nginx
-echo "EasyLEMP: Installing mysql-server..."
+echo "\e[34mEasyLEMP:\e[0m Installing mysql-server..."
 sudo apt-get install mysql-server
-echo "EasyLEMP: Installing mysql db..."
+echo "\e[34mEasyLEMP:\e[0m Installing mysql db..."
 sudo mysql_install_db
-echo "EasyLEMP: Securing mysql installation..."
+echo "\e[34mEasyLEMP:\e[0m Securing mysql installation..."
 sudo mysql_secure_installation
-echo "EasyLEMP: Installing php5-fpm..."
+echo "\e[34mEasyLEMP:\e[0m Installing php5-fpm..."
 sudo apt-get install php5-fpm
-echo "EasyLEMP: Installing php5-mysql..."
+echo "\e[34mEasyLEMP:\e[0m Installing php5-mysql..."
 sudo apt-get install php5-mysql
-read -p "EasyLEMP: Press [Enter] to configure php.ini"
+read -p "\e[34mEasyLEMP:\e[0m Press [Enter] to configure php.ini"
 sudo nano /etc/php5/fpm/php.ini
-echo "EasyLEMP: Restarting php5-fpm..."
+echo "\e[34mEasyLEMP:\e[0m Restarting php5-fpm..."
 sudo service php5-fpm restart
-read -p "EasyLEMP: Press [Enter] to configure nginx"
+read -p "\e[34mEasyLEMP:\e[0m Press [Enter] to configure nginx"
 sudo nano /etc/nginx/sites-available/default
-echo "EasyLEMP: Restarting nginx..."
+echo "\e[34mEasyLEMP:\e[0m Restarting nginx..."
 sudo service nginx restart
-read -p "EasyLEMP: Done. Press [Enter] to finish."
+read -p "\e[34mEasyLEMP:\e[0m Done. Press [Enter] to finish."
