7f9488ea9e896138a7a4cca3db84c2020657884b
Thomas Nappo <goffpaoli@gmail.com>
2015-09-07 22:18:37 -0400

    Added more automation. Also included phpMyAdmin

--- easylemp.sh
+++ easylemp.sh
@@ -9,11 +9,11 @@ sudo apt-get update
 echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing nginx...\033[0m"
 sleep 1.5
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing mysql-server...\033[0m"
 sleep 1.5
-sudo apt-get install mysql-server
+sudo apt-get -y install mysql-server
 echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing mysql db...\033[0m"
 sleep 1.5
@@ -25,28 +25,38 @@ sudo mysql_secure_installation
 echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing php5-fpm...\033[0m"
 sleep 1.5
-sudo apt-get install php5-fpm
+sudo apt-get -y install php5-fpm
 echo ""
 echo "\033[94mEasyLEMP:\033[0m Installing php5-mysql...\033[0m"
 sleep 1.5
-sudo apt-get install php5-mysql
+sudo apt-get -y install php5-mysql
 echo ""
 echo "\033[94mEasyLEMP:\033[0m Configuring php5-fpm defaults...\033[0m"
 sleep 1.5
 sudo rm -f /etc/php5/fpm/php.ini
 sudo wget https://raw.githubusercontent.com/Jire/EasyLEMP/master/defaults/php.ini -O /etc/php5/fpm/php.ini
-echo ""
 echo "\033[94mEasyLEMP:\033[0m Restarting php5-fpm...\033[0m"
 sleep 1.5
 sudo service php5-fpm restart
-echo ""
 echo "\033[94mEasyLEMP:\033[0m Configuring nginx defaults...\033[0m"
 sleep 1.5
 sudo rm -f /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/Jire/EasyLEMP/master/defaults/default -O /etc/php5/fpm/default
-echo ""
+echo "\033[94mEasyLEMP:\033[0m Adding default index and php.info page...\033[0m"
+sudo rm -f /usr/share/nginx/html/index.html
+sudo wget https://raw.githubusercontent.com/Jire/EasyLEMP/master/defaults/index.html -O /usr/share/nginx/html/index.html
+sudo wget https://raw.githubusercontent.com/Jire/EasyLEMP/master/defaults/info.php -O /usr/share/nginx/html/info.php
 echo "\033[94mEasyLEMP:\033[0m Restarting nginx...\033[0m"
 sleep 1.5
 sudo service nginx restart
+echo "\033[94mEasyLEMP:\033[0m Installing phpMyAdmin...\033[0m"
+sudo apt-get -y install phpmyadmin
+sudo ln -s /usr/share/phpmyadmin /usr/share/nginx/html
+sudo php5enmod mcrypt
+echo "\033[94mEasyLEMP:\033[0m Restarting php5-fpm...\033[0m"
+sudo service php5-fpm restart
 echo ""
-read -p "\033[94mEasyLEMP:\033[0m Done. Press \033[1m[Enter]\033[0m to finish.\033[0m"
+echo "\033[94mEasyLEMP:\033[0m We're all done!"
+echo "\033[94mEasyLEMP:\033[0m your web files can be put in /usr/share/nginx/html"
+echo "\033[94mEasyLEMP:\033[0m We've set a default index page for you and an info.php page."
+echo "\033[94mEasyLEMP:\033[0m I hope you enjoyed using \033[94mEasyLEMP\033[0m!"
